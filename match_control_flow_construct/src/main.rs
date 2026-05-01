9b83f1e8507ce585f8fc4601c17c7c6b1fe8a52d
lfrallon <loretofrallon@gmail.com>
2026-05-01 13:30:21 +0800

    chore: created match_control_flow_construct rust code project using cargo

---
+++ match_control_flow_construct/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let coin = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("The value of the coin is {} cents.", coin);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("The value of six is {:?}.", six);
+    let none = plus_one(None);
+    println!("The value of none is {:?}.", none);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+}
+
+fn add_fancy_hat() {}
+
+fn remove_fancy_hat() {}
