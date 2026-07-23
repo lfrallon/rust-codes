457355aca2fadddc1506a01b63d957d9ce8ef3e2
lfrallon <loretofrallon@gmail.com>
2026-07-23 19:53:01 +0800

    refactor: updated patterns rust project to use refutability & pattern syntax

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,5 +1,36 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct PointTwo {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+enum MessageTwo {
+        Hello { id: i32 },
+    }
+
 fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("Current location: ({x}, {y})");
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {y}");
 }
 
 fn main() {
@@ -42,4 +73,128 @@ fn main() {
 
     let point = (3, 5);
     print_coordinates(&point);
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}");
+        }
+        _ => (),
+    }
+
+    foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {setting_value:?}");
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}");
+        }
+    }
+
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{s:?}");
+
+    let origin = PointTwo { x: 0, y: 0, z: 0 };
+
+    match origin {
+        PointTwo { x, .. } => println!("x is {x}"),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    let msg = MessageTwo::Hello { id: 5 };
+
+    match msg {
+        MessageTwo::Hello { id: id @ 3..=7 } => {
+            println!("Found an id in range: {id}")
+        }
+        MessageTwo::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        MessageTwo::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
